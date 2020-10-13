55894a208c661b7101d9952afe5cb8cb52d8eb21
Yauhenii <yauhenmardan@gmail.com>
2020-10-13 23:38:11 +0300

    refactor

--- src/main/java/com/yauhenii/main/MainApplication.java
+++ src/main/java/com/yauhenii/main/MainApplication.java
@@ -1,17 +1,11 @@
 package com.yauhenii.main;
 
-import com.yauhenii.main.model.sheet.service.ExcelSheetService;
-import java.awt.EventQueue;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MainApplication {
-
-    @Autowired
-    public static ExcelSheetService excelSheetService;
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = new SpringApplicationBuilder(
